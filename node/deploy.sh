b0bb0d5a7a3c7fd46b0f7ed0be74d0c3cf8dbc77
Charmelionag <muradu.iurie.1986@gmail.com>
2020-02-12 08:52:19 +0100

    3 node kafka cluster

--- node/deploy.sh
+++ node/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo $ID > /root/kafka/data/zookeeper/myid
+echo $hostname -> id.$ID -> $(hostname -I)
 echo -e "\n172.19.0.10 node-1\n172.19.1.10 node-2\n172.19.2.10 node-3" >> /etc/hosts
 sed "/127.0.0.1/c\127.0.0.1\tlocalhost\t$HOSTNAME\tnode-$ID" /etc/hosts
-bash
-/root/kafka/bin/zookeeper-server-start.sh /root/kafka/config/zookeeper.properties
+sed -i -e "s/^broker\.id.*/broker\.id=$ID/" -e "s/^advertised\.listeners.*/advertised.listeners=PLAINTEXT:\/\/node-$ID:9092/" /root/kafka/config/server.properties
+/root/kafka/bin/zookeeper-server-start.sh /root/kafka/config/zookeeper.properties &
+/root/kafka/bin/kafka-server-start.sh /root/kafka/config/server.properties
